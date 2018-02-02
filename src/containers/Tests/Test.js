19c4de2713373fe75c6d57c473db57740762bf51
Fernando Maymone <fernando.maymone@sefaz.ce.gov.br>
2018-02-02 16:24:30 -0300

    Adding to firebase ok. Not real time

--- src/containers/Tests/Test.js
+++ src/containers/Tests/Test.js
@@ -1,18 +1,100 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { injectIntl, intlShape } from 'react-intl';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import { Activity } from 'rmw-shell'
+import { ResponsiveMenu } from 'material-ui-responsive-menu';
+import { setDialogIsOpen } from 'rmw-shell/lib/store/dialogs/actions';
+import CompanyForm from '../../components/Forms/CompanyForm';
+import { withRouter } from 'react-router-dom';
+import firebase from 'firebase';
+import FontIcon from 'material-ui/FontIcon';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import { withFirebase } from 'firekit-provider'
+import FireForm from 'fireform'
+import { change, submit } from 'redux-form';
+import isGranted from 'rmw-shell/lib/utils/auth';
+import firestore from 'firebase/firestore'
+
+
+const path = '/companies/';
+const form_name = 'company';
+
 
 class Test extends Component {
+    state = { 
+        bets: ''
+     }
+    
 
-    componentWillMount() {
-        console.log('Test will mount-----------------------------');
-        
+    handleButton = () => {
+    
+        const { firebaseApp }= this.props
+        let firestore=firebaseApp.firestore()
+        const docRef=firestore.collection('bets')
+        console.log(docRef)
+
+        docRef.add({
+            title: 'lindo match'
+        })
     }
-    constructor(props) {
-        super(props);
-        this.state = {  }
+
+    getRealTimeUpdates = () =>{
+
+        const { firebaseApp }= this.props
+        let firestore=firebaseApp.firestore()
+        const docRef=firestore.collection('bets')
+
+        docRef.onSnapshot(function(doc) {
+            if(doc && doc.exists){
+                console.log("Current bets: ", doc && doc.data());
+            }
+            
+        });
+
+
     }
-    render() { 
-        return ( <h1>Test</h1> )
-    }
+
+
+  render() {
+
+    const {
+      history,
+      intl,
+      setDialogIsOpen,
+      dialogs,
+      match,
+      submit,
+      muiTheme,
+      isGranted,
+      firebaseApp
+    } = this.props;
+    this.getRealTimeUpdates();
+
+    return (
+      <Activity>
+        <h1>Teste</h1>
+        <div style={{ margin: 15, display: 'flex' }}>
+            <FlatButton label='botao foda'  onClick={this.handleButton} />
+        </div>
+      </Activity>
+    );
+  }
 }
- 
-export default Test;
+
+
+const mapStateToProps = (state) => {
+  const { intl, dialogs } = state;
+
+  return {
+    intl,
+    dialogs,
+    isGranted: grant => isGranted(state, grant)
+  };
+};
+
+export default connect(
+  mapStateToProps, { setDialogIsOpen, change, submit }
+)(injectIntl(withRouter(withFirebase(muiThemeable()(Test)))));
