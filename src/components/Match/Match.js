501f69f1a6dba78ab6396902950cefebeab98797
Fernando Maymone <fernando.maymone@sefaz.ce.gov.br>
2018-03-21 11:40:37 -0300

    ADD added forms at match

--- src/components/Match/Match.js
+++ src/components/Match/Match.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Paper from "material-ui/Paper";
 //import Flag from "react-flags";
 import Team from "./Team";
+import { Field, reduxForm, formValueSelector } from 'redux-form';
 
 import PropTypes from "prop-types";
 import { Grid, Row, Col } from 'react-flexbox-grid';
@@ -9,23 +10,36 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 class Match extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      away_team: "0",
+      home_team: "0",
+      away_score: "0",
+      home_score: "0"
+    };
 
   }
-
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
   render() {
 
     return (
 
       <Grid fluid>
-        <Row>
+        <form onSubmit={this.handleSubmit}>
           <Col xs={6} md={3}>
             <Team team={this.props.home} home={true} />
           </Col>
           <Col xs={6} md={3}>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </Col>
+          <Col xs={6} md={3}>
             <div><Team team={this.props.away} home={false} /></div>
           </Col>
-        </Row>
+          <input type="submit" value="Submit" />
+        </form>
+
       </Grid>
     );
   }
