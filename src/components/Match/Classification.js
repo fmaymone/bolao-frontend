fc802a8b67a3db4a150b1fd9acb3be01ce0829a3
Fernando Maymone <fernando.maymone@sefaz.ce.gov.br>
2018-04-03 11:35:58 -0300

    MOD not working adding array to state

--- src/components/Match/Classification.js
+++ src/components/Match/Classification.js
@@ -17,7 +17,7 @@ class Classification extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      classification: {}
+      classification: [{}]
     };
   }
 
@@ -31,59 +31,67 @@ class Classification extends Component {
       this.addPointsToTeams(match);
     }
   }
-  addPointsToTeams(match){
+  addPointsToTeams(match) {
 
-    
 
-    let homeTeam ={};
-    let awayTeam ={};
-    //if the team is not on state homeTeam will
-    //be the new one
-    if(!this.isTeamOnState(match.home_team)){
-      homeTeam = { }
-    }else{
-      //slice from the state
-      homeTeam = { }
-    }
-    if(!this.isTeamOnState(match.away_team)){
-      awayTeam = { }
-    }else{
-      //slice from the state
-      awayTeam = { }
-    }
     switch (this.computeResult(match)) {
 
-      
+
 
       case WIN_HOME:
-        
-      
-      break
+        let homeTeam = {};
+        let awayTeam = {};
+        //if the team is not on state homeTeam will
+        //be the new one
+        if (!this.isTeamOnState(match.home_team)) {
+          homeTeam = { id: match.home_team, handycap:{plays:1,wins:1,losts:0,draws:0,gp:match.home_score,gc:match.away_score} }
+          let arrayvar = this.state.classification.slice()
+          arrayvar.push(homeTeam)
+          this.setState({ classification: arrayvar })
+        }
+        // } else {
+        //   //slice from the state
+        //   homeTeam = this.getTeamFromState(match.home_team);
+        // }
+        // if (!this.isTeamOnState(match.away_team)) {
+        //   awayTeam = {}
+        // } else {
+        //   //slice from the state
+        //   awayTeam = {}
+        // }
+
+        break
       case WIN_AWAY:
-      
-      break
+
+        break
       case DRAW:
-      
-      break
-    
+
+        break
+
     }
 
   }
-  computeResult(match) {
-    if (match.home_score > match.away_score) {
+  computeResult(match){
+
+    if(match.home_score > match.away_score){
       return WIN_HOME;
     }
-    if (match.home_score < match.away_score) {
+    if(match.home_score < match.away_score){
       return WIN_AWAY;
     }
-
-    return DRAW;
+    
+      return DRAW;
+    
+  }
+  getTeamFromState(id) {
+    return this.state.classification.find(id);
 
   }
+
   isTeamOnState(id) {
     var json = this.state.classification;
     id in json ? true : false
-    
+
   }
   render() {
 
