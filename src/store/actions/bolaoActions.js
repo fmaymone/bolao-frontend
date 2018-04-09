bebea9d4c8c9d8dbce9e3dbc03bd4b1713fb4500
Fernando Maymone <fmaymone@gmail.com>
2018-04-09 19:35:06 -0300

    ADD add firebase

--- src/store/actions/bolaoActions.js
+++ src/store/actions/bolaoActions.js
@@ -6,26 +6,23 @@ import {
   MATCHES_FETCH_SUCCESS,
   MATCH_SAVE_SUCCESS,
   MATCHES_INITIAL_CREATE
-} from "./types"
+} from "./types";
 
-import {firebaseApp} from '../../firebase';
-import firebase from 'firebase';
-
-
+import { firebaseApp } from "../../firebase";
+import firebase from "firebase";
 
 export const changeStage = group => {
   return {
     type: CHANGE_CURRENT_STAGE,
     payload: group
-  }
-}
+  };
+};
 
 export const getCurrentStage = () => {
   return {
-    type: GET_CURRENT_STAGE,
-    
-  }
-}
+    type: GET_CURRENT_STAGE
+  };
+};
 export const matchUpdate = ({ prop, value }) => {
   return {
     type: MATCH_UPDATE,
@@ -33,36 +30,56 @@ export const matchUpdate = ({ prop, value }) => {
   };
 };
 
-export const matchesInitialCreate = ( matches ) => {
+export const matchesInitialCreate = matches => {
   const { currentUser } = firebase.auth();
 
-  return (dispatch) => {
-    firebaseApp.database().ref(`/users/${currentUser.uid}/matches`)
+  return dispatch => {
+    firebaseApp
+      .database()
+      .ref(`/users/${currentUser.uid}/matches`)
       .set(matches)
       .then(() => {
         dispatch({ type: MATCHES_INITIAL_CREATE });
-        
       });
   };
 };
 
-export const updateMatch = (match) => {
+export const updateMatch = match => {
+  const { currentUser } = firebase.auth();
+  let newState = {};
 
-  return {
-    type: MATCH_UPDATE,
-    payload: match
+  return dispatch => {
+    if (currentUser) {
+      firebase
+        .database()
+        .ref(`/users/${currentUser.uid}/matches`)
+        .on("value", snapshot => {
+          newState = { ...snapshot.val() };
+          const indexToUpdate = newState.findIndex(
+            k => k.name == match.payload.name
+          );
+          newState.matches.matches[indexToUpdate] = match;
+        });
+      firebaseApp
+        .database()
+        .ref(`/users/${currentUser.uid}/matches`)
+        .set(newState)
+        .then(() => {
+          dispatch({ type: MATCH_UPDATE, payload: match });
+        });
+    }
   };
-
-}
-export const updateGroupMatch = ( group, matches ) => {
+};
+export const updateGroupMatch = (group, matches) => {
   const { currentUser } = firebase.auth();
 
-  return (dispatch) => {
-    firebaseApp.database().ref(`/users/${currentUser.uid}/matches/groups`)
+  return dispatch => {
+    firebaseApp
+      .database()
+      .ref(`/users/${currentUser.uid}/matches/groups`)
       .set(matches)
       .then(() => {
         dispatch({ type: MATCHES_INITIAL_CREATE });
-        
       });
   };
 };
@@ -79,30 +96,24 @@ export const updateGroupMatch = ( group, matches ) => {
 //   }
 //   console.log(returnValues);
 //   return returnValues;
-  
 
 // }
 
-
-
-
 export const matchesFetch = () => {
-  
   const { currentUser } = firebase.auth();
 
-  return (dispatch) => {
-    if(currentUser){
-      firebase.database().ref(`/users/${currentUser.uid}/matches`)
-        .on('value', snapshot => {
-          dispatch({ type: MATCHES_FETCH_SUCCESS, payload: {matches: snapshot.val()} });
-          console.log(snapshot.val().groups);
+  return dispatch => {
+    if (currentUser) {
+      firebase
+        .database()
+        .ref(`/users/${currentUser.uid}/matches`)
+        .on("value", snapshot => {
+          dispatch({
+            type: MATCHES_FETCH_SUCCESS,
+            payload: { matches: snapshot.val() }
+          });
+         // console.log(snapshot.val().groups);
         });
     }
   };
-  
 };
-
-
-
-
-
